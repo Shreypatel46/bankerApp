3257d67bb599a58cdf57d1e1dcb6417814b39238
Shreypatel46 <shreyp2046@gmail.com>
2024-03-04 14:34:00 +0530

    s2

--- bankerApp/src/App.jsx
+++ bankerApp/src/App.jsx
@@ -1,11 +1,35 @@
+import styles from './style';
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>
-      App
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden text-white'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}` } >
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
     </div>
-  )
-}
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+          Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+          Stats
+          business
+          billing
+          carddeal
+          testimonials
+          clients
+          cta
+          footer
+      </div>
+    </div>
+
+  </div>
+
+);
 
 export default App
