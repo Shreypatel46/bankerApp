1a50cf77c42e7f70a89758762ab167454bcd1c7c
Shreypatel46 <shreyp2046@gmail.com>
2024-03-05 00:05:43 +0530

    s2

--- bankerApp/src/App.jsx
+++ bankerApp/src/App.jsx
@@ -1,30 +1,29 @@
 import styles from './style';
-import React from 'react'
-
+import { Navbar, Hero, Stats, Business , Billing ,CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 const App = () => (
   <div className='bg-primary w-full overflow-hidden text-white'>
     <div className={`${styles.paddingX} ${styles.flexCenter}` } >
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar/>
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero/>
       </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-          Stats
-          business
-          billing
-          carddeal
-          testimonials
-          clients
-          cta
-          footer
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
       </div>
     </div>
 
--- bankerApp/src/components/CardDeal.jsx
+++ bankerApp/src/components/CardDeal.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { card } from '../assets';
+import styles, {layout} from '../style';
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>
-      CardDeal
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+         Find a better Card deal <br className='sm:block hidden'/>
+          in Few Easy Steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] pt-5`}>
+        Arcu tortor, purus in mattis at sed intefer faucibus. Aliquet quis eget mauris tortor.
+      </p>
+      <Button styles="mt-10"  />
     </div>
-  )
-}
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className='w-[100%] h-[100%]'/>
+
+    </div>
+  </section>
+)
 
 export default CardDeal
